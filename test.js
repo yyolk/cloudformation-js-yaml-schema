1b9989cf03e1ca8905491904b95e71784bd4eb76
Joseph Chiocchi <input.output@gmail.com>
2016-12-26 01:52:01 -0600

    fix test, remove extra opts

--- test.js
+++ test.js
@@ -7,10 +7,10 @@ fs.readFile(path.join(__dirname, 'test.yml'), 'utf8', function (error, data) {
   var loaded;
 
   if (!error) {
-    loaded = yaml.load(data, { schema: CLOUDFORMATION_SCHEMA, styles: { "!Ref": { canonical: "!Ref", camelcase: "!Ref" } } });
+    loaded = yaml.load(data, { schema: CLOUDFORMATION_SCHEMA });
     console.log(util.inspect(loaded, false, 20, true));
-    console.log(yaml.dump(loaded, {noRefs:true, flowLevel: 4, styles: { "!Ref": { canonical: "!Ref", camelcase: "!Ref" } }, noCompatMode: false, schema: CLOUDFORMATION_SCHEMA}))
-    newloaded = yaml.load(yaml.dump(loaded, {flowLevel: 3, styles: { "!Ref": { canonical: "!Ref" } }, schema: CLOUDFORMATION_SCHEMA}), {schema: CLOUDFORMATION_SCHEMA});
+    console.log(yaml.dump(loaded, { schema: CLOUDFORMATION_SCHEMA }));
+    newloaded = yaml.load(yaml.dump(loaded, { schema: CLOUDFORMATION_SCHEMA}), {schema: CLOUDFORMATION_SCHEMA});
     console.log(util.inspect(newloaded, false, 20, true));
   } else {
     console.error(error.stack || error.message || String(error));
