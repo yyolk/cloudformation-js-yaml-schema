050cfe1c4501981aa271b5551849955780627535
Joseph Chiocchi <joseph@gopangea.com>
2016-12-25 18:56:19 -0600

    scoping issues, temp workaround

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const yaml = require('js-yaml');
 const localTags = require('./tags.json');
 
-function Model(name) {
-  return function (data) {
-    this.class = name;
-    this.name = name;
-    this.data = data;
-  };
-}
-
-function CustomYamlType(name, kind) {
-  let model = new Model(name);
-  return new yaml.Type('!'+name, {
-    kind: kind,
-    instanceOf: model,
-    construct: function(data) {
-      return new model(data);
-    },
-    represent: function(ref, style) {
-      return ref.data;
-    }
-  });
-}
-
-function build() {
+function build(localTags,yaml) {
+  function Model(name) {
+    return function (data) {
+      this.class = name;
+      this.name = name;
+      this.data = data;
+    };
+  }
+  function CustomYamlType(name, kind) {
+    let model = Model(name);
+    return new yaml.Type('!'+name, {
+      kind: kind,
+      instanceOf: model,
+      construct: function(data) {
+        return new model(data);
+      },
+      represent: function(ref, style) {
+        return ref.data;
+      }
+    });
+  }
   let cloudformationTags = [];
-  Object.keys(localTags).map((kind) => localTags[kind].map((tag) => cloudformationTags.push(CustomYamlType(tag, kind))));
+  Object.keys(localTags).map((kind) => localTags[kind].map((tag) => cloudformationTags.push(new CustomYamlType(tag, kind))));
   return cloudformationTags;
 }
 
-module.exports.tags = cloudformationTags = build();
+let cloudformationTags = build(localTags, yaml);
+module.exports.localTags = localTags;
+module.exports.build = build;
+module.exports.cloudformationTags = cloudformationTags;
 module.exports.CLOUDFORMATION_SCHEMA = yaml.Schema.create(cloudformationTags);
+module.exports.genSchema = function(yaml) {
+  let tags = build(localTags, yaml);
+  return yaml.Schema.create(tags);
+};
