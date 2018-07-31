8a61880c5ea037a3062a67343561792d205a3652
studds <daniel.studds@gmail.com>
2018-07-31 14:56:42 +1000

    adding types

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare module 'cloudformation-js-yaml-schema' {
+    export const localTags: {mapping: string[]; scalar: string[]; sequence: string[]};
+    export const CLOUDFORMATION_SCHEMA: any;
+}
